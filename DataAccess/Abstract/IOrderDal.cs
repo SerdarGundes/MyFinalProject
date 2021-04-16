ee2ebb57f39e71e8ec221292caac7aa9ff23dc2f
Serdar Gündeş <warofpanda_main@hotmail.com>
2021-04-16 17:41:24 +0300

    16/04/2021

--- DataAccess/Abstract/IOrderDal.cs
+++ DataAccess/Abstract/IOrderDal.cs
@@ -1,11 +1,12 @@
-﻿using Entities.Concrete;
+﻿using Core.DataAccess;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface ICostumerDal:IEntityRepository<Customer>
+    public interface IOrderDal:IEntityRepository<Order>
     {
     }
 }
